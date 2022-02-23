10997078abf1239e51042c5cc7af31e662047a3c
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2022-02-23 11:17:08 -0800

    Created Left/Right Brackets Tokens

---
+++ LeftCurlyBracketToken.java
@@ -0,0 +1,13 @@
+public class LeftCurlyBracketToken implements Token {
+    public boolean equals(final Object other) {
+        return other instanceof LeftCurlyBracketToken;
+    }
+
+    public int hashCode() {
+        return 6;
+    }
+
+    public String toString() {
+        return "{";
+    }
+}
