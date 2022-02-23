a4de404607fd2248d334a103c5d61f9bac3268b8
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2022-02-23 11:16:36 -0800

    Created If/Else tokens

---
+++ ElseToken.java
@@ -0,0 +1,13 @@
+public class ElseToken implements Token {
+    public boolean equals(final Object other) {
+        return other instanceof ElseToken;
+    }
+
+    public int hashCode() {
+        return 5;
+    }
+
+    public String toString() {
+        return "else";
+    }
+}
---
+++ IfToken.java
@@ -0,0 +1,13 @@
+public class IfToken implements Token {
+    public boolean equals(final Object other) {
+        return other instanceof IfToken;
+    }
+
+    public int hashCode() {
+        return 4;
+    }
+
+    public String toString() {
+        return "if";
+    }
+}
