86050dad48f0300a9363d8bea261c13dee889a79
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2022-02-28 11:17:48 -0800

    junit extension and TokenizerTest edit
    Not completed yet(?). Will do later

--- TokenizerTest.java
+++ TokenizerTest.java
@@ -1,22 +1,39 @@
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class TokenizerTest {
     //Checks for empty strings
-    public static void testEmptyString() throws TokenizerException {
+    @Test
+    public void testEmptyString() throws TokenizerException {
         Tokenizer tokenizer = new Tokenizer("");
         List<Token> tokens = tokenizer.tokenize();
-        assert(tokens.size() == 0);
+        assertEquals(0, tokens.size());
+        //assert(tokens.size() == 0);
     }
 
     //checks for white spaces
-    public static void testOnlyWhiteSpace() throws TokenizerException {
+    @Test
+    public void testOnlyWhiteSpace() throws TokenizerException {
         Tokenizer tokenizer = new Tokenizer("    ");
         List<Token> tokens = tokenizer.tokenize();
-        assert(tokens.size() == 0);
+        assertEquals(0, tokens.size());
+        //assert(tokens.size() == 0);
     }
     
-    public static void main(String[] args) throws TokenizerException {
+    @Test
+    public void testTrueByItself() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("true");
+        List<Token> tokens = tokenizer.tokenize();
+        assertEquals(1, tokens.size());
+        Token truetoken = tokens.get(0);
+        assertTrue(truetoken instanceof TrueToken);
+    }
+
+    /*public static void main(String[] args) throws TokenizerException {
         testOnlyWhiteSpace();
         testEmptyString();
-    }
+    }*/
 }
