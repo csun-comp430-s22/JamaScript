b75264d7c9bba5eb628c3e880c0c1ffffe144dc9
Alan Hernandez <alanhp2@hotmail.com>
2022-03-02 12:02:42 -0800

    tranferred files from lexer branch

--- jamascript/src/test/java/com/jamascript/TokenizerTest.java
+++ jamascript/src/test/java/com/jamascript/TokenizerTest.java
@@ -1,24 +1,44 @@
 package com.jamascript;
-import java.util.List;
-import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class TokenizerTest {
+
+    @Test
+    public void assertTokenizes(final String input, final Token[] expected) {
+        try {
+            final Tokenizer tokenizer = new Tokenizer(input);
+            final List<Token> received = tokenizer.tokenize();
+            assertArrayEquals(expected, received.toArray(new Token[received.size()]));
+        } catch(final TokenizerException e) {
+            fail("Tokenizer threw exception");
+        }
+    }
+
     //Checks for empty strings
     @Test
-    public void testEmptyString() throws TokenizerException{
-        Tokenizer tokenizer = new Tokenizer("");
-        List<Token> tokens = tokenizer.tokenize();
-        assertTrue(tokens.size() == 0);
+    public void testEmptyString() {
+        assertTokenizes("", new Token[0]);
     }
 
-    @Test
     //checks for white spaces
-    public void testOnlyWhiteSpace() throws TokenizerException {
-        Tokenizer tokenizer = new Tokenizer("    ");
-        List<Token> tokens = tokenizer.tokenize();
-        assertTrue(tokens.size() == 0);
+    @Test
+    public void testOnlyWhiteSpace() {
+        assertTokenizes("    ", new Token[0]);
+    }
+    
+    @Test
+    public void testTrueByItself() {
+        assertTokenizes("true", new Token[] {new TrueToken()});
     }
 
+    //true true
+    @Test
+    public void testTrueSpaceTrueAreTrueTokens() {
+        assertTokenizes("true true", new Token[] {new TrueToken(), new TrueToken()});
+    }
 }
