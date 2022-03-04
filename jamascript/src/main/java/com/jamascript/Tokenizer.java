bf960010b45116d3995527b1b77dc535e19e4c5f
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2022-03-03 17:53:12 -0800

    Added [] Tokens and additional tests

--- jamascript/src/main/java/com/jamascript/Tokenizer.java
+++ jamascript/src/main/java/com/jamascript/Tokenizer.java
@@ -39,6 +39,22 @@ public class Tokenizer {
                 return new IfToken();
             } else if(name.equals("else")) {
                 return new ElseToken();
+            } else if(name.equals("Boolean")) {
+                return new BooleanToken();
+            } else if(name.equals("class")) {
+                return new ClassToken();
+            } else if(name.equals("extends")) {
+                return new ExtendsToken();
+            } else if(name.equals("Int")) {
+                return new IntToken();
+            } else if(name.equals("new")) {
+                return new NewToken();
+            } else if(name.equals("return")) {
+                return new ReturnToken();
+            } else if(name.equals("String")) {
+                return new StringToken();
+            } else if(name.equals("while")) {
+                return new WhileToken();
             } else {
                 return new VariableToken(name);
             }
@@ -67,6 +83,45 @@ public class Tokenizer {
                 } else if(input.startsWith("}", offset)) {
                     offset += 1;
                     retval = new RightCurlyBracketToken();
+                } else if(input.startsWith("[", offset)) {
+                    offset += 1;
+                    retval = new LeftSquaredBracketToken();
+                } else if(input.startsWith("]", offset)) {
+                    offset += 1;
+                    retval = new RightSquaredBracketToken();
+                } else if(input.startsWith("/", offset)) {
+                    offset += 1;
+                    retval = new DivideToken();
+                } else if(input.startsWith(",", offset)) {
+                    offset += 1;
+                    retval = new CommaToken();
+                } else if(input.startsWith(".", offset)) {
+                    offset += 1;
+                    retval = new DotToken();
+                } else if(input.startsWith(">", offset)) {
+                    offset += 1;
+                    retval = new GreaterThanToken();
+                } else if(input.startsWith("<", offset)) {
+                    offset += 1;
+                    retval = new LessThanToken();
+                } else if(input.startsWith("-", offset)) {
+                    offset += 1;
+                    retval = new MinusToken();
+                } else if(input.startsWith("*", offset)) {
+                    offset += 1;
+                    retval = new MultiplyToken();
+                } else if(input.startsWith("!", offset)) {
+                    offset += 1;
+                    retval = new NotToken();
+                } else if(input.startsWith("+", offset)) {
+                    offset += 1;
+                    retval = new PlusToken();
+                } else if(input.startsWith("\"", offset)) {
+                    offset += 1;
+                    retval = new QuotationMarkToken();
+                } else if(input.startsWith(";", offset)) {
+                    offset += 1;
+                    retval = new SemicolonToken();
                 } else {
                     throw new TokenizerException();
                 }
